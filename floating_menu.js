5eca44d3ce58f6c4888a45c4c0012ce417ada875
mtaz337 <51666409+mtaz337@users.noreply.github.com>
2022-08-25 12:44:04 +0600

    Create floating_menu.js

---
+++ floating_menu.js
@@ -0,0 +1,67 @@
+const FloatingMenuComponent = {
+    isLoading: true,
+    leftNav: [
+        {
+            countryCode: 'usa',
+            link: 'USA',
+            img: '../image/store/usa.png'
+        },
+        {
+            countryCode: 'uk',
+            link: 'UK',
+            img: '../image/store/uk.png'
+        },
+        {
+            countryCode: 'cad',
+            link: 'Canada',
+            img: '../image/store/canada.png'
+        }
+    ],
+    rightNav: [
+        {
+            link: 'Price Calculator',
+            disabled: true,
+            img: '../image/store/usa/price_.webp'
+        },
+        {
+            link: 'Check Pricing',
+            disabled: true,
+            img: '../image/store/usa/check2.png'
+        },
+        {
+            link: 'Demanding Brands',
+            disabled: true,
+            img: '../image/store/usa/brands_1.jpg'
+        },
+        // {
+        //     link: 'LiveShopr',
+        //     disabled: true,
+        //     img: '../image/store/liveShoper.png'
+        // }
+    ],
+
+    init() {
+        let self = this
+        setTimeout(function () {
+            self.isLoading = false
+        }, 1000)
+    },
+
+    countryCodeFromLocation() {
+        let queryString = window.location.search
+        if (!queryString) return 'usa'
+
+        let urlParams = new URLSearchParams(queryString)
+        let countryCode = urlParams.get("countryCode")
+        return countryCode
+    },
+
+    gotoStore(countryCode) {
+        if (countryCode === 'usa') {
+            window.location = "/"
+            return
+        }
+
+        window.location = "/stores/?countryCode=" + countryCode
+    }
+}
